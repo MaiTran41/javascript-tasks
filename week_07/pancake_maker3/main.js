f4cc160117ab6ab82c40fa5799ce76445c53957f
MaiTran41 <maitran.040196@gmail.com>
2025-03-18 15:00:16 +0200

    Add pancake solutions

--- week_07/pancake_maker3/main.js
+++ week_07/pancake_maker3/main.js
@@ -11,6 +11,13 @@ const summaryText = document.getElementById("summaryText");
 const confirmOrderBtn = document.getElementById("confirmOrder");
 
 const changeHandler = () => {
+  const { basePrice, toppingsTotal, extraTotal, deliveryPrice } =
+    calculateTotalPrice();
+
+  displayPriceText({ basePrice, toppingsTotal, extraTotal, deliveryPrice });
+};
+
+const calculateTotalPrice = () => {
   const basePrice = parseFloat(
     document.getElementById("type").selectedOptions[0].dataset.price
   );
@@ -28,12 +35,21 @@ const changeHandler = () => {
     ...document.querySelectorAll(".delivery:checked"),
   ].reduce((sum, delivery) => sum + parseFloat(delivery.dataset.price), 0);
 
-  totalPriceDisplay.textContent = `${
+  return { basePrice, toppingsTotal, extraTotal, deliveryPrice };
+};
+
+const displayPriceText = ({
+  basePrice,
+  toppingsTotal,
+  extraTotal,
+  deliveryPrice,
+}) => {
+  const priceText = `${
     basePrice + toppingsTotal + extraTotal + deliveryPrice
   }€`;
-  totalPriceBanner.textContent = `${
-    basePrice + toppingsTotal + extraTotal + deliveryPrice
-  }€`;
+
+  totalPriceDisplay.textContent = priceText;
+  totalPriceBanner.textContent = priceText;
 };
 
 pancakeForm.addEventListener("change", changeHandler);
@@ -41,23 +57,40 @@ pancakeForm.addEventListener("change", changeHandler);
 const getToppings = () => {
   const selectedToppings = document.querySelectorAll(".topping:checked");
 
-  const toppingList = [];
+  const toppingNameList = [];
 
   selectedToppings.forEach((element) => {
-    toppingList.push(
+    toppingNameList.push(
       element.parentElement.innerText.trim().toLocaleLowerCase()
     );
   });
-  return toppingList.join(", ");
+  return toppingNameList;
 };
+
+const getFormattedToppingsText = () => {
+  const toppingNameList = getToppings();
+
+  return toppingNameList.join(", ");
+};
+
 const getExtras = () => {
   const selectedExtras = document.querySelectorAll(".extra:checked");
 
-  const extraList = [];
+  const extraNameList = [];
 
   selectedExtras.forEach((element) => {
-    extraList.push(element.parentElement.innerText.trim().toLocaleLowerCase());
+    const [formattedExtra] = element.parentElement.innerText
+      .trim()
+      .toLocaleLowerCase()
+      .split(" - ");
+    extraNameList.push(formattedExtra);
   });
+
+  return extraNameList;
+};
+
+const getFormattedExtrasText = () => {
+  const extraList = getExtras();
 
   return extraList.join(", ");
 };
@@ -65,38 +98,36 @@ const getExtras = () => {
 const getDelivery = () => {
   const selectedDeliveryMethod = document.querySelector(".delivery:checked");
 
-  const formattedSelectedDeliveryMethod =
-    selectedDeliveryMethod.parentElement.innerText.trim();
+  const [formattedSelectedDeliveryMethod] =
+    selectedDeliveryMethod.parentElement.innerText
+      .trim()
+      .toLowerCase()
+      .split(" (");
   return formattedSelectedDeliveryMethod;
 };
 
-// Check this
+const getPancakeType = () => {
+  const [formattedPancakeType] = pancakeType.value.split(" - ");
+  return formattedPancakeType;
+};
+
 const seeOrderClickHandler = () => {
-  const toppings = getToppings();
-  const extras = getExtras();
+  const toppings = getFormattedToppingsText();
+  const extras = getFormattedExtrasText();
   const deliveryMethod = getDelivery();
 
-  // array destructuring
-  const [formattedPancakeType] = pancakeType.value.split(" - ");
+  const pancakeType = getPancakeType();
 
-  console.log(formattedPancakeType);
-
-  const [formattedExtras] = extras.split(" -");
-
-  // Example: "Order created by Mei: Blueberries pancake  with [nuts, syrup] & cream. Delivery method: "
-  summaryText.textContent = `Order created by ${customerName.value}: ${formattedPancakeType} pancake with ${toppings} & ${formattedExtras}. Delivery method: ${deliveryMethod} `;
+  summaryText.textContent = `Order created by ${customerName.value}: ${pancakeType} pancake with ${toppings} & ${extras}. Delivery method: ${deliveryMethod} `;
 };
 
 seeOrderBtn.addEventListener("click", seeOrderClickHandler);
-
-// Step 3 :
-const orders = [];
 
 const confirmOrderHandler = () => {
   const order = {
     id: Date.now(),
     customerName: customerName.value,
-    // selectedPancake: formattedPancakeType,
+    pancakeType: getPancakeType(),
     toppings: getToppings(),
     extras: getExtras(),
     deliveryMethod: getDelivery(),
